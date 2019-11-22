7a625ff314a97f7a4806372bb44f2669bf9b36c3
Sune Kirkeby <mig@ibofobi.dk>
2019-11-22 09:51:09 +0100

    silly impl

--- reqeasy/src/client.rs
+++ reqeasy/src/client.rs
@@ -2,17 +2,34 @@
 
 use crate::error::Result;
 
+use std::io::prelude::*;
+
+/// FIXME
+pub enum Protocol { HTTP, HTTPS }
+
 /// FIXME
 //FIXME
 #[allow(unused_variables,dead_code)]
 pub struct RequestBuilder {
-    url: String,
     method: String,
+    protocol: Protocol,
+    host: String,
+    path: String,
 }
 
 impl RequestBuilder {
+    // FIXME remove unwraps
     pub fn from_url(url: &str) -> Result<Self> {
-        Ok(RequestBuilder { url: url.into(), method: "GET".into() })
+        let url = ::url::Url::parse(url)?;
+        // FIXME remove asserts; check other fields
+        assert!(url.scheme() == "http");
+        assert!(url.query().is_none());
+        Ok(RequestBuilder {
+            method: "GET".into(),
+            protocol: Protocol::HTTP,
+            host: url.host_str().unwrap().into(),
+            path: url.path().into(),
+        })
     }
 
     pub fn method(mut self, method: &str) -> Self {
@@ -20,11 +37,27 @@ impl RequestBuilder {
     }
 
     pub fn send(self) -> Result<Response> {
-        unimplemented!()
+        let mut c = ::std::net::TcpStream::connect((self.host.as_ref(), 80u16))?;
+        c.write(format!(
+            "GET {} HTTP/1.0\r\nHost: {}\r\n\r\n",
+            self.path, self.host,
+        ).as_ref())?;
+        c.shutdown(::std::net::Shutdown::Write)?;
+        Response::read_from(&mut ::std::io::BufReader::new(c))
     }
 }
 
 /// FIXME
 pub struct Response {
     pub status_code: u16 //FIXME
+}
+
+impl Response {
+    fn read_from<B: ::std::io::BufRead>(c: &mut B) -> Result<Self> {
+        let mut line = String::new();
+        c.read_line(&mut line)?;
+        dbg!(&line);
+        let status = line.split(' ').nth(1).unwrap().parse().unwrap();
+        Ok(Response { status_code: status })
+    }
 }
--- reqeasy/src/error.rs
+++ reqeasy/src/error.rs
@@ -2,3 +2,17 @@
 pub struct Error {}
 
 pub type Result<T> = ::std::result::Result<T, Error>;
+
+impl From<::url::ParseError> for Error {
+    fn from(_: ::url::ParseError) -> Error {
+        //FIXME
+        Error {}
+    }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(_: ::std::io::Error) -> Error {
+        //FIXME
+        Error {}
+    }
+}
