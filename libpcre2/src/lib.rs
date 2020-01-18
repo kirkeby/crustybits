590dff089a6dfdbbf0860a9d8002eb369fcfa6f6
Sune Kirkeby <mig@ibofobi.dk>
2020-01-18 08:26:52 +0100

    Do the API the right way.

--- libpcre2/src/lib.rs
+++ libpcre2/src/lib.rs
@@ -1,32 +1,59 @@
 use std::ffi::c_void;
+use std::ptr::null;
 
 #[link(name="pcre2-8", kind="dylib")]
 extern "C" {
-    pub fn pcre2_compile_8(
+    fn pcre2_compile_8(
         pattern: *const u8, len: usize, opts: u32,
         errno: *mut i32, errpos: *mut usize,
         ctx: *const c_void
     ) -> *const c_void;
+
+    fn pcre2_code_free_8(code: *const c_void);
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Error {
+    pub errno: i32,
+    pub offset: usize,
+}
+
+/// `Code` represents a compiled regular expression.
+pub struct Code {
+    code: *const c_void,
+}
+
+impl Code {
+    /// Compile a string into a regular expression.
+    pub fn compile(s: &str) -> Result<Self, Error> {
+        let mut err = Error::default();
+        let code = unsafe {
+            pcre2_compile_8(
+                s.as_ptr(), s.len(), 0,
+                &mut err.errno as *mut i32, &mut err.offset as *mut usize,
+                null() as *const c_void
+            )
+        };
+        if code.is_null() {
+            return Err(err);
+        }
+        Ok(Code { code })
+    }
+}
+
+impl Drop for Code {
+    fn drop(&mut self) {
+        unsafe { pcre2_code_free_8(self.code); }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::pcre2_compile_8;
-    use std::ffi::c_void;
-    use std::ptr::null;
+    use crate::{Code, Error};
 
     #[test]
-    fn it_works() {
-        let pattern = "Hello, (.*)!";
-        let mut errno: i32 = 0;
-        let mut errpos: usize = 0;
-        let code = unsafe {
-            pcre2_compile_8(
-                pattern.as_ptr(), pattern.len(), 0,
-                &mut errno as *mut i32, &mut errpos as *mut usize,
-                null() as *const c_void
-            )
-        };
-        assert!(!code.is_null());
+    fn it_works() -> Result<(), Error> {
+        let _c = Code::compile("Hello, (.*)!")?;
+        Ok(())
     }
 }
